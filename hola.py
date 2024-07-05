7034d25e5af36ed86a3e1547ddc4ce85be1bc69a
Brayam Programador <brateomx@gmail.com>
2024-07-04 18:50:18 -0600

    primer commit

---
+++ hola.py
@@ -0,0 +1,31 @@
+print("Hola")
+
+print("Hola")
+
+name = 'Brayam'
+caracter = "B"
+dato = '''Brayam 
+
+con 
+
+
+comillas'''
+
+las_name = '  Teofilo jimenez '
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[-1])
+print(dato)
+
+
+print(name +" "+ las_name)
+
+print(las_name * 5)
+
+print(len(name))
+print(len(las_name))
+
+print(name.lower())
+print(name.upper())
+print(las_name.strip())
