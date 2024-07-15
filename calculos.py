fd2e9384ddb37fd70325259699d1596915e250c2
Brayam Programador <brateomx@gmail.com>
2024-07-15 12:41:41 -0600

    tipos de archivos

---
+++ calculos.py
@@ -0,0 +1,17 @@
+#este es un comentario 
+
+x = 10
+y = 5.10641
+z = 1.2e9
+a = 5.6e-6
+
+is_true = True
+is_false = False
+print(type(x))
+print(type(y))
+print(type(z))
+print(z)
+print(a)
+print(is_true)
+print(is_false)
+
