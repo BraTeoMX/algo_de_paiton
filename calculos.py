5846af7bee5d3a0e228a3ff69d2f9fced9cdeba8
Brayam Programador <brateomx@gmail.com>
2024-07-16 15:50:15 -0600

    guardado

--- calculos.py
+++ calculos.py
@@ -15,3 +15,20 @@ print(a)
 print(is_true)
 print(is_false)
 
+
+
+frase = "Algo random"
+autor = "Brayam"
+
+print("Frase:", frase, "Autor:", autor)
+print(f"Frase: {frase}, Autor: {autor}")
+print("Frase: {}, Autor: {}".format(frase, autor))
+
+
+valor = 2.659416
+print("valor: {:.2f}".format(valor))
+print("Hola mundo: \n{:.2f}".format(valor))
+print('Hola soy \'Brayam\'')
+
+print("la ruta de archivo es:\n C:\\Users\\Usuario\\Desktop\\archivo.txt")
+
